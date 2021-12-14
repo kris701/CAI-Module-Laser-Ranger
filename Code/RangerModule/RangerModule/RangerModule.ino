ae52502e1b1a0d83ebb1697cdb8edb161da3647b
Kristian Skov Johansen <kris701kj@gmail.com>
2021-12-14 10:58:41 +0100

    Added base code

---
+++ Code/RangerModule/RangerModule/RangerModule.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RangerModule.ino
+ Created:	12/14/2021 10:58:20 AM
+ Author:	kris7
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
